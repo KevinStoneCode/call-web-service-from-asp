9f6702c84e6969071103c388115bd0a664eddf47
KevinStone <kwshih0212@gmail.com>
2016-10-11 13:03:34 +0800

    change Namespace
    change Namespace

--- TemperatureWebService/App_Code/Convert.vb
+++ TemperatureWebService/App_Code/Convert.vb
@@ -4,7 +4,7 @@ Imports System.Web.Services.Protocols
 
 ' 若要允許使用 ASP.NET AJAX 從指令碼呼叫此 Web 服務，請取消註解下列一行。
 ' <System.Web.Script.Services.ScriptService()> _
-<WebService(Namespace:="http://tempuri.org/")> _
+<WebService(Namespace:="http://tempuri1.org/")> _
 <WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
 <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Public Class Convert
